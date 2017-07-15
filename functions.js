0ff2257bdab3a0e0f55f79aff95752f6f2d20c75
aliciasavelly <alicia.savelly@gmail.com>
2017-07-15 16:49:16 -0700

    improve formatting

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
 NOTES_IDX = 2;
 
 function newNote() {
-  $("#notes-section").append(`<div id="note-${NOTES_IDX}" class='note'><button type="button" name="button" class="x-button">X</button></div>`);
+  $("#notes-section").append(`<div id="note-${NOTES_IDX}" class='note'><p>${NOTES_IDX}</p><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div>`);
   NOTES_IDX += 1;
+}
+
+function deleteNote(id) {
+  $(`#${id}`).remove();
 }
