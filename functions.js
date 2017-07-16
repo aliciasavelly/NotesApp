6c33adaa4833166dfd4e0a84a6f1a07e4a189348
aliciasavelly <alicia.savelly@gmail.com>
2017-07-15 18:10:03 -0700

    add new note button adds proper type of note

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 NOTES_IDX = 2;
 
 function newNote() {
-  $("#notes-section").prepend(`<div id="note-${NOTES_IDX}" class='note'><div class="button-holder"><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div><p>${NOTES_IDX}</p></div>`);
+  $("#notes-section").prepend(`<div id="note-${NOTES_IDX}" class='note'><div class="button-holder"><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div><span placeholder='Type here...' contenteditable='true'></span></div>`);
   NOTES_IDX += 1;
 }
 
