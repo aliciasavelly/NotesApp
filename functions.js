9cefe3852dba08b65893142935cca0b4d6a99af1
aliciasavelly <alicia.savelly@gmail.com>
2017-07-15 18:27:33 -0700

    create color changing buttons and dynamically set color to stay the same for new notes

--- functions.js
+++ functions.js
@@ -1,10 +1,16 @@
 NOTES_IDX = 2;
+CURRENT_NOTE_COLOR = "#fdffaa";
 
 function newNote() {
-  $("#notes-section").prepend(`<div id="note-${NOTES_IDX}" class='note'><div class="button-holder"><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div><span placeholder='Type here...' contenteditable='true'></span></div>`);
+  $("#notes-section").prepend(`<div id="note-${NOTES_IDX}" class='note' style="background-color:${CURRENT_NOTE_COLOR}"><div class="button-holder"><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div><span placeholder='Type here...' contenteditable='true'></span></div>`);
   NOTES_IDX += 1;
 }
 
 function deleteNote(id) {
   $(`#${id}`).remove();
+}
+
+function changeNoteColor(color) {
+  $(".note").css("background-color", color);
+  CURRENT_NOTE_COLOR = color;
 }
