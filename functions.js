090ffd813e11ab5f161d7ac68c1b71ea6f947f81
aliciasavelly <alicia.savelly@gmail.com>
2017-07-15 18:07:29 -0700

    work on formatting, add span to type in which scrolls

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 NOTES_IDX = 2;
 
 function newNote() {
-  $("#notes-section").append(`<div id="note-${NOTES_IDX}" class='note'><p>${NOTES_IDX}</p><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div>`);
+  $("#notes-section").prepend(`<div id="note-${NOTES_IDX}" class='note'><div class="button-holder"><button type="button" name="button" class="x-button" onClick="deleteNote('note-${NOTES_IDX}')">X</button></div><p>${NOTES_IDX}</p></div>`);
   NOTES_IDX += 1;
 }
 
